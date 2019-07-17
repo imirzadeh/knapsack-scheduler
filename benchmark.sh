a9d5acc06f149f6e04bb62659a60bfbf6018072f
Iman Mirzadeh <seyediman.mirzadeh@wsu.edu>
2019-07-16 19:10:04 -0700

    Fix module call error in benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@ for ID in $(seq 0 $(echo $(ls -lh models/*.joblib | wc -l)-1 | bc))
 do
     for N in 1 2 3
     do
-        sudo likwid-powermeter python3 ./knapsack/main.py $ID > $ID_out.txt
+        sudo likwid-powermeter python3 -m knapsack.main $ID > $ID_out.txt
         cat $ID_out.txt | grep 'PKG' -A 1 | tail -n 1 | cut -d ' ' -f 3 >> $ID.txt
         cat $ID_out.txt | grep "Runtime" | cut -d ' ' -f 2  >> $ID.txt
         rm $ID_out.txt
